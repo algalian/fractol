f04e02bca7081b5e27be5043851444a68a7de98e
Alvaro Galiana Albores <algalian@c1r12s4.42madrid.com>
2023-10-05 18:37:10 +0200

    alt render

--- test_circle.c
+++ test_circle.c
@@ -3,6 +3,15 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+typedef struct s_img
+{
+    void	*mlx_img;
+    char	*addr;
+    int		bpp; /* bits per pixel */
+    int		line_len;
+    int		endian;
+}	t_img;
+
 
 typedef struct s_data
 {
@@ -10,27 +19,30 @@ typedef struct s_data
     void	*mlx_win;
     int     w;
     int     h;
+    t_img  img;
 }	t_data;
+
 
 int close(t_data *data)
 {   
     /*printf("%p\n", data->mlx);
     printf("%p\n", data->mlx_win);*/
-    mlx_destroy_window(data->mlx, data->mlx_win);
+    mlx_clear_window(data->mlx,data->mlx_win);
+   // mlx_destroy_window(data->mlx, data->mlx_win);
     printf("ventana destruida\n");
     return(0);
 }
+
+
 int render(t_data *data)
 {
     int x;
     int y;
     float r;
 
-    data->h = 500;
-    data->w = 500;
     x = 0;
     y = data->h;
-    r = 250;
+    r = data->h/2;
     while(y >= 0)
     {
         x = 0;
@@ -57,16 +69,16 @@ int	main(void)
         free(data.mlx);
         return(1);
     }
-    data.w = 500;
-    data.h = 500;
+    data.w = 800;
+    data.h = 800;
 	data.mlx_win = mlx_new_window(data.mlx, data.w, data.h, "Una ventana");
     if(!data.mlx_win)
     {
         free(data.mlx_win);
         return(1);
     }
-    data.w = 500;
-    data.h = 500;
+    data.img.mlx_img = mlx_new_image(data.mlx, data.w, data.h);
+    data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
     mlx_loop_hook(data.mlx, &render, &data);
     mlx_key_hook(data.mlx_win, &close, &data);
     mlx_loop(data.mlx);
