b0d521724b62fa1beee6adf62dff4defffe2e263
Alvaro Galiana Albores <algalian@c1r12s4.42madrid.com>
2023-10-05 17:22:15 +0200

    destroy segfault

--- test_circle.c
+++ test_circle.c
@@ -8,65 +8,67 @@ typedef struct s_data
 {
     void	*mlx;
     void	*mlx_win;
+    int     w;
+    int     h;
 }	t_data;
 
-
-int	handle_no_event(void *data)
-{
-    void    *dummy;
-
-    dummy = data;
-    return (0);
-}
-int handle_input(int keysym, t_data data)
+int close(t_data *data)
 {   
-    int dummy;
+    /*printf("%p\n", data->mlx);
+    printf("%p\n", data->mlx_win);*/
+    mlx_destroy_window(data->mlx, data->mlx_win);
+    printf("ventana destruida\n");
+    return(0);
+}
+int render(t_data *data)
+{
+    int x;
+    int y;
+    float r;
 
-    dummy = keysym;
-	mlx_destroy_window(data.mlx, data.mlx_win);
-    return (0);
+    data->h = 500;
+    data->w = 500;
+    x = 0;
+    y = data->h;
+    r = 250;
+    while(y >= 0)
+    {
+        x = 0;
+        while(x <= data->w)
+        {
+            if(sqrt (pow (abs (x - (data->w/2)),2) + pow (abs (y - (data->h/2)),2)) <= r)
+            {
+                mlx_pixel_put(data->mlx, data->mlx_win, x, y, 0xFF0000);
+            }
+            x++;
+        }
+        y--;
+    }
+    return(0);
 }
 
 int	main(void)
 {
     t_data  data;
-    //int     x;
-    //int     y;
-    int     w;
-    int     h;
-    //float   r;
 
 	data.mlx = mlx_init();
     if(!data.mlx)
+    {
+        free(data.mlx);
         return(1);
-    w = 500;
-    h = 500;
-	data.mlx_win = mlx_new_window(data.mlx, w, h, "The abyss");
+    }
+    data.w = 500;
+    data.h = 500;
+	data.mlx_win = mlx_new_window(data.mlx, data.w, data.h, "Una ventana");
     if(!data.mlx_win)
     {
         free(data.mlx_win);
         return(1);
     }
-    /*x = 0;
-    y = h;
-    r = 250;
-    while(y >= 0)
-    {
-        x = 0;
-        while(x <= w)
-        {
-            if(sqrt (pow (abs (x - (w/2)),2) + pow (abs (y - (h/2)),2)) <= r)
-            {
-                mlx_pixel_put(data.mlx,data.mlx_win,x,y,0xFF0000);
-            }
-            x++;
-        }
-        y--;
-    }*/
-    mlx_loop_hook(data.mlx, &handle_no_event, &data);
-    //mlx_key_hook(data.mlx_win, &handle_input, &data);
-    mlx_hook(data.mlx_win, 2, 1L<<0, &handle_input, &data);
+    data.w = 500;
+    data.h = 500;
+    mlx_loop_hook(data.mlx, &render, &data);
+    mlx_key_hook(data.mlx_win, &close, &data);
     mlx_loop(data.mlx);
-   // mlx_destroy_display(data.mlx);
-
+    return(0);
 }
