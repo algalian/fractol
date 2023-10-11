2391daf7749088cba25da945b06deb78deb1b762
Alvaro Galiana Albores <algalian@c2r1s5.42madrid.com>
2023-10-11 20:08:13 +0200

    rendering tests

--- render.c
+++ render.c
@@ -7,7 +7,7 @@ typedef struct s_img
 {
     void	*mlx_img;
     char	*addr;
-    int		bpp; /* bits per pixel */
+    int		bpp;
     int		line_len;
     int		endian;
 }	t_img;
@@ -19,35 +19,35 @@ typedef struct s_data
     void	*mlx_win;
     int     w;
     int     h;
-    float   rank;
+    double   rank;
     t_img  img;
 }	t_data;
 
 
-int fractal_set(float a, float b)
+
+int fractal_set(double a, double b)
 {
-    float   x;
-    float   y;
-    float   m;
+    double  x;
+    double  y;
+    double  m;
     int     p;
 
     x = a;
     y = b;
-    m = sqrt(x*x + y*y);
+    m = 0;
     p = 0;
-    while(m <= 2 && p <= 300)
+    while(fabs(m) <= 2 && p <= 1000)
     {
-        a = a*a - b*b;
-        b = 2*a*b;
-        a += x;
-        b += y;
+        a = ((a*a) - (b*b)) + x;
+        b = (2*a*b) + y;
         m = sqrt(a*a + b*b);
         p++;
     }
-    if(p < 300)
+    if(p < 1000)
         return(0);
     return(1);
 }
+
 
 int close(t_data *data)
 {   
@@ -69,33 +69,28 @@ void	img_pix_put(t_img *img, int x, int y, int color)
     *(int *)pixel = color;
 }
 
-
-
 int render(t_data *data)
 {
     int x;
     int y;
-    float a;
-    float b;
+    double a;
+    double b;
 
-    x = 0;
     y = 0;
-    a = -2;
-    b = 2;
-    while(y <= data->h)
+    b = data->rank/2;
+    while(y <= data->h && b >= -2)
     {
         x = 0;
-        while(x <= data->w)
+        a = -data->rank/2;
+        while(x <= data->w && a <= 2)
         {
-            if(fractal_set(a,b) == 1)
-            {
-                img_pix_put(&data->img,  x, y, 0xFF0000);
-            }
+            if(fractal_set(a,b) == 0)
+                img_pix_put(&data->img,  x, y, 0x0000FF);
             x++;
             a += (data->rank/data->h);
         }
         y++;
-        b += (data->rank/data->w);
+        b -= (data->rank/data->w);
     }
     mlx_put_image_to_window(data->mlx, data->mlx_win, data->img.mlx_img, 0, 0);
     return(0);
--- sets.c
+++ sets.c
@@ -1,43 +1,71 @@
+#include<complex.h>
 #include<math.h>
 #include<stdio.h>
 
-
-
-int fractal_set(float a, float b)
+int fractal_set(double a, double b)
 {
-    float   x;
-    float   y;
-    float   m;
+    double  x;
+    double  y;
+    double  m;
     int     p;
+    FILE    *file;
 
+    fopen("somerule.txt","w");
     x = a;
     y = b;
-    m = sqrt(x*x + y*y);
+    m = 0;
     p = 0;
-    while(m <= 2 && p <= 300)
+    while(fabs(m) <= 2 && p <= 1000)
     {
-        a = a*a - b*b;
-        b = 2*a*b;
-        a += x;
-        b += y;
+        a = ((a*a) - (b*b)) + x;
+        b = (2*a*b) + y;
         m = sqrt(a*a + b*b);
+        fprintf(file,"some rule a:%f b: %f m: %f\n",a,b,m);
         p++;
     }
-    if(p < 300)
+    fclose(file);
+    if(p < 1000)
         return(0);
     return(1);
 }
 
-
+int is_in_mandelbrot(double complex c)
+{
+    double complex z;
+    FILE    *file;
+    fopen("mandelbrot.txt","w");
+    
+    z = 0;
+    for (int i = 0; i < 1000; i++) 
+    {
+        z = z * z + c;
+        fprintf(file,"Mandelbrot a:%f b:%f m:%f\n", creal(z),cimag(z),cabs(z));
+        if (cabs(z) > 2) 
+        {
+            fclose(file);
+            return 0;  
+        }
+    }
+    fclose(file);
+    return 1; 
+}
 
 int main()
 {
-    float a;
-    float b;
+    double a;
+    double b;
+    double complex c;
+    int i;
 
     a = 1;
-    b = 0;
-    printf("%f\n",a);
-    printf("%i\n",fractal_set(a,b));
-    return(0);
+    b = -1;
+    c = a + b*I;
+   is_in_mandelbrot(c);
+   fractal_set(a,b);
+
 }
+
+
+
+
+
