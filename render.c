0f24774809c8851294ee419c00a8f36275e33b16
Alvaro Galiana Albores <algalian@c3r14s1.42madrid.com>
2023-10-16 22:38:20 +0200

    zoom and move

--- render.c
+++ render.c
@@ -5,154 +5,147 @@
 
 typedef struct s_img
 {
-    void	*mlx_img;
-    char	*addr;
-    int		bpp;
-    int		line_len;
-    int		endian;
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
 }	t_img;
 
+typedef struct move
+{
+    double	a;
+    double	b;
+    double	scale;
+} move;
 
 typedef struct s_data
 {
-    void	*mlx;
-    void	*mlx_win;
-    int     w;
-    int     h;
-    double  rank;
-    t_img  img;
+	void	*mlx;
+	void	*mlx_win;
+	int     w;
+	int     h;
+	float	rank_w;
+	float	rank_h;
+	t_img  img;
+	move	m;
 }	t_data;
 
 typedef struct complex
 {
-    float a;
-    float b;
+	double a;
+	double b;
 } complex;
 
 complex   ft_csum(complex x, complex y)
 {
-    complex z;
-    z.a = x.a + y.a;
-    z.b = x.b + y.b;
-    return(z);
+	complex z;
+	z.a = x.a + y.a;
+	z.b = x.b + y.b;
+	return(z);
 }
+
 complex ft_csqrt(complex x)
 {
-    complex z;
+	complex z;
 
-    z.a = x.a * x.a - x.b * x.b;
-    z.b = 2 * x.a * x.b;
-    return(z);    
+	z.a = x.a * x.a - x.b * x.b;
+	z.b = 2 * x.a * x.b;
+	return(z);    
 }
-
 
 int fractal_set(complex c)
 {
-    int     p;
-    complex z;
+	int     p;
+	complex z;
 
-    z.a = 0;
-    z.b = 0;
-    p = 0;
-    while(p <= 1000)
-    {
-        z = ft_csqrt(z);
-        z = ft_csum(z,c);
-        if(hypot(z.a, z.b)>2)
-            return(0);
-        p++;
+	z.a = 0;
+	z.b = 0;
+	p = 0;
+	while(p <= 1000)
+	{
+		z = ft_csqrt(z);
+		z = ft_csum(z,c);
+		if(hypot(z.a, z.b)>2)
+			return(0);
+		p++;
 
-    }
-    return(1);
+	}
+	return(1);
 }
 
-/*int is_in_mandelbrot(float complex c)
+void close(t_data data)
 {
-    float complex z;
-    int i;
-    
-    i = 0;
-    z = 0;
-    while ( i < 1000) 
-    {
-        z = z * z + c;
-        if (cabsf(z) > 2) 
-            return(0);
-        i++;
-    }
-    return (1); 
-}*/
+	mlx_destroy_window(data.mlx,data.mlx_win);
 
-int close(t_data *data)
+}
+t_data key_event(t_data *data)
 {   
 
-    /*printf("%p\n", data->mlx);
-    printf("%p\n", data->mlx_win);*/
-    mlx_clear_window(data->mlx,data->mlx_win);
-    //printf("%d\n",mlx_clear_window(data->mlx,data->mlx_win));
-    //mlx_destroy_window(data->mlx, data->mlx_win);
-    printf("ventana destruida\n");
-    return(0);
-
+	if()
+		data->m.a += ;
+	if()
+		data->m.b -= ;
+	if() 
+		data->m.b += ;
+	if()
+		data->m.b -= 1 
+	if() 
+		close(data);
+	return(t_data);
 }
+
 void	img_pix_put(t_img *img, int x, int y, int color)
 {
-    char    *pixel;
-    
-    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
-    *(int *)pixel = color;
+	char    *pixel;
+	
+	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(int *)pixel = color;
 }
 
 int render(t_data *data)
 {
-    int x;
-    int y;
-    complex c;
+	int x;
+	int y;
+	complex c;
 
-
-    y = 0;
-    c.b = data->rank/2;
-    while(y <= data->h && c.b >= -2)
-    {
-        x = 0;
-        c.a = -data->rank/2;
-        while(x <= data->w && c.a <= 2)
-        {
-            if(fractal_set(c) == 0)
-                img_pix_put(&data->img,  x, y, 0x0000FF);
-            x++;
-            c.a += (data->rank/data->h);
-        }
-        y++;
-        c.b -= (data->rank/data->w);
-    }
-    mlx_put_image_to_window(data->mlx, data->mlx_win, data->img.mlx_img, 0, 0);
-    return(0);
+	y = 0;
+	c.b = ((data->rank_h/2) + data->m.a) * data->m.scale;
+	while(y <= data->h || c.b >= -2 * data->m.scale)
+	{
+		x = 0;
+		c.a = ((-data->rank_h/2) + data->m.b) * data->m.scale;
+		while(x <= data->w || c.a <= 2 * data->m.scale)
+		{
+			if(fractal_set(c) == 1)
+				img_pix_put(&data->img,  x, y, 0x0000FF);
+			x++;
+			c.a += (data->rank_h/data->w)*data->m.scale;
+		}
+		y++;
+		c.b -= (data->rank_h/data->h)*data->m.scale;
+	}
+	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img.mlx_img, 0, 0);
+	return(0);
 }
 
 int	main()
 {
-    t_data  data;
+	t_data  data;
 
 	data.mlx = mlx_init();
-    if(!data.mlx)
-    {
-        free(data.mlx);
-        return(1);
-    }
-    data.w = 800;
-    data.h = 800;
-    data.rank = 4;
+	data.w = 800;
+	data.h = 800;
+	data.rank_h = 4;
+	data.rank_w = 4;
+	data.m.scale = 1;
+	data.m.a = 0;
+	data.m.b = 0;
 	data.mlx_win = mlx_new_window(data.mlx, data.w, data.h, "Una ventana");
-    if(!data.mlx_win)
-    {
-        free(data.mlx_win);
-        return(1);
-    }
-    data.img.mlx_img = mlx_new_image(data.mlx, data.w, data.h);
-    data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
-    mlx_loop_hook(data.mlx, &render, &data);
-    mlx_key_hook(data.mlx_win, &close, &data);
-    mlx_loop(data.mlx);
-    return(0);
+	data.img.mlx_img = mlx_new_image(data.mlx, data.w, data.h);
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
+	mlx_loop_hook(data.mlx, &render, &data);
+	mlx_key_hook(data.mlx_win, &key_event, &data);
+	mlx_loop(data.mlx);
+	return(0);
 }
