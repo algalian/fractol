bdb888efe0470108a1415d5da900154cdf6d4076
Alvaro Galiana Albores <algalian@c3r5s6.42madrid.com>
2023-10-25 19:11:48 +0200

    rápido

--- render.c
+++ render.c
@@ -1,5 +1,14 @@
+/**
+ * The above code is a program that generates and displays a fractal image using the Mandelbrot set.
+ * 
+ * @param x The parameter "x" is used as a variable to iterate over the x-axis of the image being
+ * rendered. It represents the current pixel position along the x-axis.
+ * @param y The variable "y" is used as a counter for the y-coordinate of the pixel being rendered. It
+ * starts at 0 and increments by 1 until it reaches the height of the image.
+ * 
+ * @return The main function is returning an integer value of 0.
+ */
 #include<mlx.h>
-#include<math.h>
 #include<stdlib.h>
 #include<stdio.h>
 #include<stdbool.h>
@@ -13,6 +22,7 @@
 #include"X11/Xlib.h"
 #include"X11/Xutil.h"
 #include"X11/tkIntXlibDecls.h"
+#include<math.h>
 
 
 
@@ -32,6 +42,12 @@ typedef struct move
     double	scale;
 } move;
 
+typedef struct complex
+{
+	double a;
+	double b;
+} complex;
+
 typedef struct s_data
 {
 	void	*mlx;
@@ -43,21 +59,17 @@ typedef struct s_data
 	t_img  	img;
 	move	m;
 	int		max_iter;
+	complex c;
 }	t_data;
 
-typedef struct complex
-{
-	double a;
-	double b;
-} complex;
 
 int render(t_data *data);
 
-complex   ft_csum(complex x, complex y)
+complex   ft_csum(complex x, complex *y)
 {
 	complex z;
-	z.a = x.a + y.a;
-	z.b = x.b + y.b;
+	z.a = x.a + y->a;
+	z.b = x.b + y->b;
 	return(z);
 }
 
@@ -70,7 +82,7 @@ complex ft_csqrt(complex x)
 	return(z);    
 }
 
-bool fractal_set(t_data *data, complex c)
+bool fractal_set(t_data *data)
 {
 	int     p;
 	complex z;
@@ -81,8 +93,8 @@ bool fractal_set(t_data *data, complex c)
 	while(p <= data->max_iter)
 	{
 		z = ft_csqrt(z);
-		z = ft_csum(z,c);
-		if(hypot(z.a, z.b)>2)
+		z = ft_csum(z,&data->c);
+		if(sqrt(z.a*z.a + z.b*z.b)>2)
 			return(false);
 		p++;
 	}
@@ -105,17 +117,17 @@ int key_event(int keycode, t_data *data)
 	data->img.mlx_img = mlx_new_image(data->mlx,data->w,data->h);
 	data->img.addr = mlx_get_data_addr(data->img.mlx_img, &data->img.bpp, &data->img.line_len, &data->img.endian);
 	if(keycode == 0 || keycode == 123)
-		data->m.a = data->m.a - 0.05 * data->m.scale;
+		data->m.a -= 0.15 * (2 * data->m.scale/0.95);
 	if(keycode == 2 || keycode == 124)
-		data->m.a =data->m.a + 0.05 * data ->m.scale;
+		data->m.a += 0.15 * (2 * data->m.scale/0.95);
 	if(keycode == 13 || keycode == 126)
-		data->m.b = data->m.b + 0.05 * data->m.scale;
+		data->m.b += 0.15 * (2 * data->m.scale/0.95);
 	if(keycode == 1 || keycode == 125)
-		data->m.b = data->m.b - 0.05 * data->m.scale;
+		data->m.b -= 0.15 * (2 * data->m.scale/0.95);
 	if(keycode == 69)
-		data->m.scale = data->m.scale * 0.95;// *  pow(10, ft_brake(data));
+		data->m.scale *= 0.95;
 	if(keycode == 78)
-		data->m.scale = data->m.scale * 1.05;
+		data->m.scale *= 1.05;
 	return(0);
 }
 
@@ -129,27 +141,26 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 
 int render(t_data *data)
 {
-	int x;
-	int y;
-	complex c;
+	int	x;
+	int	y;
 
 	y = 0;
-	c.b = data->rank_h/2* data->m.scale + data->m.b;
-	while(y < data->h)
+	data->c.b = data->rank_h/2 * data->m.scale + data->m.b;
+	while(y < data->h && data->c.b > -2)
 	{
 		x = 0;
-		c.a = -data->rank_w/2 * data->m.scale + data->m.a;
-		while(x < data->w)
+		data->c.a = -data->rank_w/2 * data->m.scale + data->m.a;
+		while(x < data->w && data->c.a < 2)
 		{
-			if(fractal_set(data,c) == 1 && (c.b > -2 && c.a < 2))
+			if(fractal_set(data) == 1)
 				img_pix_put(&data->img,  x, y, 0x0000FF);
 			if(x == data->w/2 && y == data->h/2)
 				img_pix_put(&data->img,x,y,0xFF0000);
 			x++;
-			c.a += (data->rank_w/data->w)*data->m.scale;
+			data->c.a += (data->rank_w/data->w) * data->m.scale;
 		}
 		y++; 
-		c.b -= (data->rank_h/data->h)*data->m.scale;
+		data->c.b -= (data->rank_h/data->h) * data->m.scale;
 	}
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img.mlx_img, 0, 0);
 	return(0);
@@ -164,10 +175,12 @@ int	main()
 	data.h = 800;
 	data.rank_h = 4;
 	data.rank_w = 4;
-	data.m.scale = 1;
+	data.m.scale = 0.05;
 	data.m.a = -0.79939982;
 	data.m.b = -0.16519870;
-	data.max_iter = 30;
+	data.max_iter = 100;
+	data.c.a = -2;
+	data.c.b = 2;
 	data.mlx_win = mlx_new_window(data.mlx, data.w, data.h, "Una ventana");
 	data.img.mlx_img = mlx_new_image(data.mlx, data.w, data.h);
 	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
