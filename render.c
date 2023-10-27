ff34d82da9097059378a1736921741e30775d553
Alvaro Galiana Albores <algalian@c1r3s4.42madrid.com>
2023-10-27 19:00:38 +0200

    julia & prompts

--- render.c
+++ render.c
@@ -1,4 +1,5 @@
 
+#include"mlx/mlx.h"
 #include<mlx.h>
 #include<stdlib.h>
 #include<stdio.h>
@@ -15,7 +16,11 @@
 #include"X11/tkIntXlibDecls.h"
 #include<math.h>
 
-
+# define BLACK 0x000000
+# define WHITE 0xFFFFFF
+# define RED   0xFF0000
+# define GREEN 0x00FF00
+# define BLUE  0x0000FF
 
 typedef struct s_img
 {
@@ -39,6 +44,12 @@ typedef struct complex
 	double b;
 } complex;
 
+typedef struct prompt
+{
+	char 	**choice;
+	complex c;
+} prompt;
+
 typedef struct s_data
 {
 	void	*mlx;
@@ -54,6 +65,7 @@ typedef struct s_data
 	int		in_mod;
 	int		out_mod;
 	int		color;
+	prompt 	user;
 }	t_data;
 
 complex   ft_csum(complex x, complex *y)
@@ -97,27 +109,34 @@ int key_event(int keycode, t_data *data)
 		data->m.b += 0.15 * (2 * data->m.scale/0.95);
 	if(keycode == 1 || keycode == 125)
 		data->m.b -= 0.15 * (2 * data->m.scale/0.95);
-	if(keycode == 69)
+	if((keycode == 69 || keycode == 14) && data->m.scale <= 1)
 	{
-		data->m.scale *= 0.95;
-		if(data->m.scale < 1)
-		{
-			data->in_mod++;
-			if(data->in_mod % 3 == 0)
-				data->max_iter++;
-		}
+		data->max_iter += 10;
+		printf("iterations: %d\n", data->max_iter);
 	}
-	if(keycode == 78)
+	if((keycode == 78 || keycode == 15) && data->max_iter - 10 > 0)
 	{
-		data->m.scale *= 1.05;
-		if(data->m.scale < 1)
-		{
-			data->out_mod++;
-			if(data->out_mod % 3 == 0)
-				data->max_iter--;
-		}
+		data->max_iter -= 10;
+		printf("iterations: %d\n", data->max_iter);
 	}
 	return(0);
+}
+
+int	mouse_hook(int button, int x, int y, t_data *data)
+{
+	mlx_destroy_image(data->mlx,data->img.mlx_img);
+	data->img.mlx_img = mlx_new_image(data->mlx,data->w,data->h);
+	data->img.addr = mlx_get_data_addr(data->img.mlx_img, &data->img.bpp, &data->img.line_len, &data->img.endian);
+	if(button == 4)
+		data->m.scale *= 0.95;
+	if(button == 5)
+		data->m.scale *= 1.1;
+	if(button == 1 || button == 2)
+	{
+		data->c.b = data->c.b + y * (data->rank_h/data->h);
+		data->c.a = data->c.a + x * (data->rank_w/data->w);
+	}
+	return(0); 
 }
 
 void	img_pix_put(t_img *img, int x, int y, int color)
@@ -128,23 +147,33 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 	*(unsigned int *)pixel = color;
 }
 
+
+
+
 void fractal_set(t_data *data, int x, int y)
 {
-	int     p;
+	int		p;
 	complex z;
+	int color;
 
 	z.a = 0;
 	z.b = 0;
+	julia(data,z);
 	p = 0;
-	while(p <= data->max_iter)
+	while(p < data->max_iter)
 	{
 		z = ft_csqrt(z);
 		z = ft_csum(z,&data->c);
-		if(z.a*z.a + z.b*z.b>2)
-			break;
+		if(z.a*z.a + z.b*z.b > 4)
+		{	
+			color = p*((RED - GREEN)/data->max_iter);
+			img_pix_put(&data->img,x,y,color);
+			return;
+		}
 		p++;
 	}
-	img_pix_put(&data->img, x, y, ((0xFFFFFF-0x000000)/30)*p);
+	img_pix_put(&data->img,x,y,BLACK);
+	return;
 }
 
 int render(t_data *data)
@@ -154,11 +183,11 @@ int render(t_data *data)
 
 	y = 0;
 	data->c.b = data->rank_h/2 * data->m.scale + data->m.b;
-	while(y < data->h && data->c.b > -2)
+	while(y < data->h)
 	{
 		x = 0;
 		data->c.a = -data->rank_w/2 * data->m.scale + data->m.a;
-		while(x < data->w && data->c.a < 2)
+		while(x < data->w)
 		{
 			fractal_set(data,x,y);
 			x++;
@@ -183,16 +212,17 @@ int	main()
 	data.m.scale = 1;
 	data.m.a = 0;
 	data.m.b = 0;
-	data.max_iter = 30;
+	data.max_iter = 20;
 	data.c.a = -2;
 	data.c.b = 2;
-	data.in_mod = 0;
-	data.out_mod = 0;
+	data.user.choice = "julia";
+	data.user.c = 
 	data.mlx_win = mlx_new_window(data.mlx, data.w, data.h, "Una ventana");
 	data.img.mlx_img = mlx_new_image(data.mlx, data.w, data.h);
 	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
 	mlx_hook(data.mlx_win,2, 0, &key_event, &data);
 	mlx_hook(data.mlx_win,17, 0,&close,&data);
+	mlx_mouse_hook(data.mlx_win,mouse_hook, &data);
 	mlx_loop_hook(data.mlx, &render, &data);
 	mlx_loop(data.mlx);
 	return(0);
